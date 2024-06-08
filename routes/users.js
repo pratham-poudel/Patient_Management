b022d2ab95156664271be06d28292225ce6ee315
Pratham Poudel <prathampoudel2@gmail.com>
2024-06-08 08:45:25 +0545

    fixeddd

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const plm=require('passport-local-mongoose');
 require('dotenv').config();
 const mongoURI = process.env.MONGODB_URI;
-// mongoose.connect(mongoURI).then(() => {
-//   console.log('Connected to the database');
-// });
-mongoose.connect('mongodb://127.0.0.1:27017/ganapati')
+mongoose.connect(mongoURI).then(() => {
+  console.log('Connected to the database');
+});
+// mongoose.connect('mongodb://127.0.0.1:27017/ganapati')
 
 
 const userSchema = new mongoose.Schema({
